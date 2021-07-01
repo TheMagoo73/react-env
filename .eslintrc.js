58ea2e6465386b5bc6b8c9f41590736006dee5f7
John Clarke <john.clarke@gfsdeliver.com>
2021-07-01 16:33:24 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2020: true,
+      jest: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "prettier",
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {},
+    settings: {
+      react: {
+        version: "16.8",
+      },
+    },
+  };
---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import babel from "rollup-plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import del from "rollup-plugin-delete";
+import pkg from "./package.json";
+import analyze from "rollup-plugin-analyzer";
+
+export default {
+  input: pkg.source,
+  output: [
+    {
+      /*preserveModules: false, exports: 'named',*/ file: pkg.main,
+      format: "cjs",
+    },
+    {
+      /*preserveModules: false, exports: 'named',*/ file: pkg.module,
+      format: "esm",
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    babel({ runtimeHelpers: true }),
+    commonjs(),
+    del({ targets: ["dist/*"] }),
+    analyze,
+  ],
+  external: [
+    ...Object.keys({ ...pkg.peerDependencies } || {}),
+  ],
+};
